ace9043fb29f94f16a69c7234fffaff12bac9ae7
davRic42 <davidric.2710@gmail.com>
2024-02-14 11:56:20 -0500

    pasar archivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //prueba
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+          <h1 class="navbar-brand" href="#">Parkig Crud</h1>
+        </nav>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
---
+++ frontend/src/components/forms.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function (){
+    return(
+        // comentario de prueba
+        <div className="m-4">
+            <h2>Ingreso del carro</h2>
+        </div>
+    );
+}
