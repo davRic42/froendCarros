722d23d7e7f0605c36d8a07d2007c3d60039570d
davRic42 <davidric.2710@gmail.com>
2024-02-14 13:13:16 -0500

    errores

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-          <h1 class="navbar-brand" href="#">Parkig Crud</h1>
-        </nav>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <body>
-
-      </body>
     </div>
   );
 }
